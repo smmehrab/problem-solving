cf1f96fbe8a166f89b6a2696e0046b99b762fb6a
smmehrab <mehrab.24csedu.001@gmail.com>
2020-09-08 19:07:23 +0600

    SumofRootToLeafBinaryNumbers.cpp - AC

---
+++ codes/leetcode/SumofRootToLeafBinaryNumbers.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int traverse(TreeNode* node, int sum) { 
+        if (!node) return 0;
+        sum <<= 1;
+        sum += node->val;
+        return (!node->left && !node->right) ? sum : traverse(node->left, sum) + traverse(node->right, sum);
+    }
+    
+public:
+    int sumRootToLeaf(TreeNode* root) {
+        return traverse(root, 0);
+    }
+};
