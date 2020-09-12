a3691b1caf2cc4716521a29b1cfca55cd2568573
smmehrab <mehrab.24csedu.001@gmail.com>
2020-09-12 18:35:13 +0600

    CombinationSumlll.cpp - AC

---
+++ codes/leetcode/CombinationSumlll.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    int limit, totalSum;
+    vector<int> valid;
+    vector<vector<int>> result;
+
+    void solve(int pos, int sum){
+        if(pos==limit) {
+            if(sum==totalSum) result.push_back(valid);
+            return;
+        }
+        else if(sum>=totalSum) return; 
+        
+        int start=1;
+        if(valid.size()!=0) start = valid.back()+1; 
+        for(int option = start; option <= 9; option++) {
+            valid.push_back(option);
+            solve(pos+1, sum + option);
+            valid.pop_back();
+        }
+    }
+
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        limit = k;
+        totalSum = n;
+        solve(0, 0);
+        return result;
+    }
+};
