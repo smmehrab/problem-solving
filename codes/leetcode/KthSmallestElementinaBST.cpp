96870a0970bc63d53c7bcc746aa8334a3fa3f619
smmehrab <mehrab.24csedu.001@gmail.com>
2020-09-15 16:19:25 +0600

    KthSmallestElementinaBST.cpp - AC

---
+++ codes/leetcode/KthSmallestElementinaBST.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int c = 0;
+
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        if (!root) return 0;
+        
+        int kthSmallestInLST = kthSmallest(root->left, k);
+        if (kthSmallestInLST != 0) return kthSmallestInLST;
+        
+        c++;
+        if (c == k) return root->val;
+        
+        int kthSmallestInRST = kthSmallest(root->right, k);
+        return kthSmallestInRST;
+    }
+};
