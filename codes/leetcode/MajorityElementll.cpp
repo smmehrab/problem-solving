e35f00d6df902a2c0544855c7268944ef6b2b2ff
smmehrab <mehrab.24csedu.001@gmail.com>
2020-09-23 03:14:29 +0600

    MajorityElementll.cpp - AC

---
+++ codes/leetcode/MajorityElementll.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& numbers) {
+        int majorityElement1=INT_MIN, majorityElement2=INT_MIN, frequency1=0, frequency2=0, threshold = numbers.size()/3;
+        vector<int> majorityElements;
+
+        for(int number : numbers) {
+            if(number==majorityElement1) frequency1++;
+            else if(number==majorityElement2) frequency2++;
+            else if(frequency1==0){
+                majorityElement1 = number;
+                frequency1++;
+            } 
+            else if(frequency2==0) {
+                majorityElement2 = number;
+                frequency2++;
+            }
+            else{
+                frequency1--;
+                frequency2--;
+            }
+        }
+        
+        frequency1=0;
+        frequency2=0;
+        for(int number : numbers){
+            if(number == majorityElement1) frequency1++;
+            else if(number == majorityElement2) frequency2++;
+        }
+        
+        if(frequency1 > threshold) majorityElements.push_back(majorityElement1);
+        if(frequency2 > threshold) majorityElements.push_back(majorityElement2);
+        return majorityElements;
+    }
+};
