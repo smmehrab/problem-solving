232bfda3165756beff1ba6c37505141ebd5f9e99
smmehrab <mehrab.24csedu.001@gmail.com>
2020-09-22 11:36:45 +0600

    LongestAbsoluteFilePath.cpp - AC

---
+++ codes/leetcode/LongestAbsoluteFilePath.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int lengthLongestPath(string path) {
+        path.push_back('\n');
+
+        vector<int> lengths(10000, 0);
+        int currentLevel = 0, longestPath = 0, currentLength = 0;
+        bool isLeaf = false;
+            
+        for(char character : path){
+            switch(character){
+                case '\n': 
+                    currentLevel = 0; 
+                    currentLength = 0; 
+                    isLeaf = false; 
+                    break; 
+                case '\t': 
+                    currentLevel++; 
+                    break;
+                case '.' : 
+                    isLeaf = true;
+                default:
+                    currentLength++;
+                    lengths[currentLevel] = currentLength;
+                    if(isLeaf) longestPath = max(longestPath, accumulate(lengths.begin(), lengths.begin() + currentLevel + 1, 0) + currentLevel);
+            }
+        }
+        
+        return longestPath;
+    }
+};
