4ba158c16a8c612c9593213ec0531588d8a153f5
smmehrab <mehrab.24csedu.001@gmail.com>
2020-09-24 05:48:01 +0600

    GasStation.cpp - AC

---
+++ codes/leetcode/GasStation.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int numberOfGasStations = gas.size() ;
+        int gasLeftFrom0 = 0;
+        int gasLeftFromStart = 0;
+        int startIndex = 0;
+
+        for (int i=0; i<numberOfGasStations; i++) {
+            if (gas[i]>=cost[i] && gasLeftFromStart<0) {
+                startIndex = i;
+                gasLeftFromStart = 0;
+            }
+            gasLeftFrom0 += (gas[i]-cost[i]);
+            gasLeftFromStart += (gas[i]-cost[i]);
+        }
+        
+        return (gasLeftFromStart<0 || gasLeftFrom0<0) ? -1 : startIndex;
+    }
+};
