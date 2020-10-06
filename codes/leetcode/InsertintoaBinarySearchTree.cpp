18e1d4f59d406f1b1da5a44b3e5e56ed7fdffb1a
smmehrab <mehrab.24csedu.001@gmail.com>
2020-10-06 15:07:43 +0600

    InsertintoaBinarySearchTree.cpp - AC

---
+++ codes/leetcode/InsertintoaBinarySearchTree.cpp
@@ -0,0 +1,40 @@
+	/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(!root) return new TreeNode(val);
+
+        TreeNode* current = root;
+        while(true) {
+            if(current->val<=val) {
+                if(current->right) {
+                    current = current->right;
+                } 
+                else { 
+                    current->right = new TreeNode(val); 
+                    break;
+                }
+            } 
+            else {
+                if(current->left) {
+                    current = current->left;
+                }
+                else {
+                    current->left = new TreeNode(val);
+                    break;
+                }
+            }   
+        }
+        return root;
+    }
+};
