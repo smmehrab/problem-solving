b2582e99de5c320f8f45c80b9c8764a327b0f8ac
smmehrab <mehrab.24csedu.001@gmail.com>
2020-10-12 22:39:21 +0600

    RemoveDuplicateLetters.cpp - AC

---
+++ codes/leetcode/RemoveDuplicateLetters.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string word) {
+        stack<int> letters;
+        set<int> added;
+        map<char, int> frequency;
+        for(char letter : word) frequency[letter]++;
+
+        for(char letter : word) {
+            if(added.find(letter)==added.end()) {
+                while(!letters.empty() && letter<letters.top() && frequency[letters.top()]>0) {
+                    added.erase(added.find(letters.top()));
+                    letters.pop();                    
+                }
+                letters.push(letter);
+                added.insert(letter);
+            }
+            frequency[letter]--;
+        }
+        
+        string result="";
+        while(!letters.empty()) {
+            result.push_back(letters.top());
+            letters.pop();
+        }
+        reverse(result.begin(), result.end());
+        
+        return result;
+    }
+};
