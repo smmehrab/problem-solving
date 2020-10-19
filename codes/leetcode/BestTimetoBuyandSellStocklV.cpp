edabec37ed06fff9279a551d41ac7190e634c041
smmehrab <mehrab.24csedu.001@gmail.com>
2020-10-19 16:44:53 +0600

    BestTimetoBuyandSellStocklV.cpp - AC

---
+++ codes/leetcode/BestTimetoBuyandSellStocklV.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProfit(int numberOfTransactions, vector<int>& prices) {
+        int numberOfDays = prices.size();
+        if(numberOfDays==0 || numberOfTransactions==0) return 0;
+        
+        if(numberOfTransactions>=numberOfDays/2){
+            long maxProfit=0;
+            for(int day=1; day<numberOfDays; day++)
+                if(prices[day]>prices[day-1])
+                    maxProfit += prices[day]-prices[day-1];
+            return maxProfit;
+        }
+
+        int dp[numberOfTransactions+1][numberOfDays+1];
+        memset(dp, 0, sizeof(dp));
+        for(int transaction=1; transaction<=numberOfTransactions; transaction++) {
+            int maxProfit = dp[transaction][0]-prices[0];
+            for(int day=1; day<=numberOfDays; day++) {
+                dp[transaction][day] = max(dp[transaction][day-1], prices[day-1]+maxProfit);
+                maxProfit = max(maxProfit, dp[transaction-1][day]-prices[day-1]);
+            }
+        }
+
+        return dp[numberOfTransactions][numberOfDays];
+    }
+};
