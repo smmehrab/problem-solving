0301949b3e1cd3cc7a14ca36063ab0b14651a957
smmehrab <mehrab.24csedu.001@gmail.com>
2020-10-20 16:04:30 +0600

    CloneGraph.cpp - AC

---
+++ codes/leetcode/CloneGraph.cpp
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+private:
+    Node* cloneNode(Node* node, vector<Node*>& clone) {
+        if(!node || clone[node->val]) return clone[node->val];
+        clone[node->val] = new Node(node->val);
+        for(Node* neighbor : node->neighbors) clone[node->val]->neighbors.push_back(cloneNode(neighbor, clone));
+        return clone[node->val];
+    }
+
+public:
+    Node* cloneGraph(Node* node) {
+        if(!node) return node;
+        vector<Node*> clone(110, nullptr);
+        return cloneNode(node, clone);
+    }
+};
