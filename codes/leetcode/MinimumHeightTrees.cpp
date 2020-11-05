c145c46e9ec711f226154749f69817022d3a7219
smmehrab <mehrab.24csedu.001@gmail.com>
2020-11-05 06:22:33 +0600

    MinimumHeightTrees.cpp - AC

---
+++ codes/leetcode/MinimumHeightTrees.cpp
@@ -0,0 +1,33 @@
+class Solution { 
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if(n<2) return {0};
+        
+        vector<vector<int>> graph(n);
+        vector<int> degrees(n, 0);
+        for(vector<int> edge: edges){
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+            degrees[edge[0]]++;
+            degrees[edge[1]]++;
+        }        
+     
+        vector<int> leafNodes, nextLeafNodes;
+        for(int node=0; node<=n-1; node++)
+            if(degrees[node] == 1) 
+                leafNodes.push_back(node);
+
+        while(!leafNodes.empty()) {
+            nextLeafNodes.clear();
+            for(int leafNode : leafNodes) 
+                for(int neighbor: graph[leafNode])
+                    if(--degrees[neighbor] == 1)
+                        nextLeafNodes.push_back(neighbor);
+
+            if(nextLeafNodes.empty()) break;
+            leafNodes = nextLeafNodes;
+        }
+
+        return leafNodes;
+    }
+};
