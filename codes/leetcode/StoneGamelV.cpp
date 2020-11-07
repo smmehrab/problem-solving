b7e328cfd751e815bc6a117f21880fa74a82c70f
smmehrab <mehrab.24csedu.001@gmail.com>
2020-11-07 21:49:24 +0600

    StoneGamelV.cpp - Modified

--- codes/leetcode/StoneGamelV.cpp
+++ codes/leetcode/StoneGamelV.cpp
@@ -1,17 +1,30 @@
-class Solution {
-private:
-    bool dp[100100];
-    bool solve(int remain) {
-        if(dp[remain]) return dp[remain];
-        for (int i=1; (i*i)<=remain; i++) 
-            if (!solve(remain - (i*i))) 
-                return dp[remain] = true;
-        return dp[remain];
-    }
+// class Solution {
+// private:
+//     bool dp[100100];
+//     bool solve(int remain) {
+//         if(dp[remain]) return dp[remain];
+//         for (int i=1; (i*i)<=remain; i++) 
+//             if (!solve(remain - (i*i))) 
+//                 return dp[remain] = true;
+//         return dp[remain];
+//     }
     
+// public:
+//     bool winnerSquareGame(int n) {
+//         for(int i=0; i<n+10; i++) dp[i] = false;
+//         return solve(n);
+//     }
+// };
+
+class Solution {
 public:
     bool winnerSquareGame(int n) {
-        for(int i=0; i<n+10; i++) dp[i] = false;
-        return solve(n);
+        vector<bool> dp(n+1, false);
+        for (int i=0; i<=n; i++) {
+            if (dp[i]) continue;
+            for (int j=1; i+(j*j)<=n; j++) 
+                dp[i+(j*j)] = true;
+        }
+        return dp[n];
     }
 };
