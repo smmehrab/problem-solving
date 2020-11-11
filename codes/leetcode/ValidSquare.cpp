3ee34c7ba6a708c8caa83dcf14ccdcd84c127bac
smmehrab <mehrab.24csedu.001@gmail.com>
2020-11-12 05:55:59 +0600

    ValidSquare.cpp - AC

---
+++ codes/leetcode/ValidSquare.cpp
@@ -0,0 +1,18 @@
+class Solution {
+private:
+    int distance(vector<int>& p1, vector<int>& p2){
+        return (p1[0]-p2[0])*(p1[0]-p2[0]) + (p1[1]-p2[1])*(p1[1]-p2[1]);
+    }
+
+public:
+    bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
+        set<int> uniqueDistances;
+        uniqueDistances.insert(distance(p1, p2));
+        uniqueDistances.insert(distance(p1, p3));
+        uniqueDistances.insert(distance(p1, p4));
+        uniqueDistances.insert(distance(p2, p3));
+        uniqueDistances.insert(distance(p2, p4));
+        uniqueDistances.insert(distance(p3, p4));
+        return uniqueDistances.find(0)==uniqueDistances.end() && uniqueDistances.size()==2;
+    }
+};
