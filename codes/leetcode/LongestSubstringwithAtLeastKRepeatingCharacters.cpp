19630be26b50a70e925aff0ba98dad1b24acca23
smmehrab <mehrab.24csedu.001@gmail.com>
2020-11-30 16:40:24 +0600

    LongestSubstringwithAtLeastKRepeatingCharacters.cpp - AC

---
+++ codes/leetcode/LongestSubstringwithAtLeastKRepeatingCharacters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    int solve(string &s, int start, int end, int k) {
+        if (end<k) return 0;
+
+        int frequency[26] = {0};
+        for (int i=start; i<end; i++) frequency[s[i]-'a']++;
+
+        for (int mid=start; mid<end; mid++) {
+            if (frequency[s[mid]-'a'] >= k) continue;
+            int midNext = mid+1;
+            while (midNext<end && frequency[s[midNext]-'a']<k) midNext++;
+            return max(solve(s, start, mid, k), solve(s, midNext, end, k));
+        }
+        return (end-start);
+    }
+
+public:
+    int longestSubstring(string s, int k) {
+        return solve(s, 0, s.size(), k);
+    }
+};
