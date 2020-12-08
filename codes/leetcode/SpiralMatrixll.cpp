db02a6cbe44fd232c8ff1da1fcd577e636c7fb23
smmehrab <mehrab.24csedu.001@gmail.com>
2020-12-08 17:49:41 +0600

    SpiralMatrixll.cpp - AC

---
+++ codes/leetcode/SpiralMatrixll.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    int n, number;
+    vector<vector<int>> matrix;
+    void traverse(int i, int j, char d) {
+        if(i<0 || j<0 || i>=n ||  j>=n || matrix[i][j]>0) return;
+                
+        matrix[i][j] = number++;
+        
+        if(d == 'u') traverse(i-1, j, 'u');
+        traverse(i, j+1, 'r');
+        traverse(i+1, j, 'd');
+        traverse(i, j-1, 'l');
+        traverse(i-1, j, 'u');
+    } 
+
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        this->n = n;
+        this->number = 1;
+        this->matrix = vector<vector<int>>(n, vector<int>(n, 0));
+        traverse(0, 0, 'r');
+        return matrix;
+    }
+};
