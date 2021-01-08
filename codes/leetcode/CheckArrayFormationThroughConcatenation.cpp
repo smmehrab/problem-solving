9a9038748916b404b12084b445e26dd80b4613e0
smmehrab <mehrab.24csedu.001@gmail.com>
2021-01-08 09:08:04 +0600

    CheckArrayFormationThroughConcatenation.cpp - AC

---
+++ codes/leetcode/CheckArrayFormationThroughConcatenation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canFormArray(vector<int>& arr, vector<vector<int>>& pieces) {
+        int n = arr.size(), p = pieces.size();
+        if(n==1 && n==p) return arr[0] == pieces[0][0];
+
+        map<int, int> indexOf;
+        for(int i=0; i<n; i++) indexOf[arr[i]] = i;
+        
+        bool answer = true;
+        for(vector<int> piece : pieces) {
+            p = piece.size();
+            for(int i=0; i<p-1; i++){
+                if(indexOf[piece[i]]+1 != indexOf[piece[i+1]]) {
+                    answer = false;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+};
