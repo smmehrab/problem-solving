a5504ae96b1c82fb726c5c23b7c618d96f0e44f8
smmehrab <mehrab.24csedu.001@gmail.com>
2021-01-08 11:20:20 +0600

    LongestUnivaluePath.cpp - AC

---
+++ codes/leetcode/LongestUnivaluePath.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private: 
+    int longest;
+    int findSubtreeLongest(TreeNode* node) {
+        if (!node) return 0;
+        
+        int leftSubtreeLongest = findSubtreeLongest(node->left);
+        int rightSubtreeLongest = findSubtreeLongest(node->right);
+        
+        int leftLongest = (node->left && node->left->val == node->val) ? leftSubtreeLongest + 1 : 0;
+        int rightLongest = (node->right && node->right->val == node->val) ? rightSubtreeLongest + 1 : 0;
+        
+        longest = max(longest, leftLongest + rightLongest);
+        return max(leftLongest, rightLongest);
+    }
+    
+public:
+    int longestUnivaluePath(TreeNode* root) {
+        longest = 0;
+        int temp = findSubtreeLongest(root);
+        return longest;
+    }
+};
