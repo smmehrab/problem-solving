023afce4238ed9f506b60cddbd2886a8cff95cd5
smmehrab <mehrab.24csedu.001@gmail.com>
2021-01-09 07:11:39 +0600

    PathSumlll.cpp - Modified

--- codes/leetcode/PathSumlll.cpp
+++ codes/leetcode/PathSumlll.cpp
@@ -1,22 +1,19 @@
 class Solution {
 private:
-    int solve(TreeNode* root, int sum, int targetSum, unordered_map<int, int>& dp) {
+    int helper(TreeNode* root, int sum){
         if(!root) return 0;
-
-        sum += root->val;
-        int ret = dp[sum-targetSum];
-        dp[sum]++;
-        ret += solve(root->left, sum, targetSum, dp);
-        ret += solve(root->right, sum, targetSum, dp);
-        dp[sum]--;
-        
-        return ret;
+        int count = 0;
+        if(root->val == sum) count++;
+        count += helper(root->left, sum-root->val);
+        count += helper(root->right, sum-root->val);
+        return count;
     }
-
+    
 public:
-    int pathSum(TreeNode* root, int targetSum) {
-        unordered_map<int, int> dp;
-        dp[0] = 1;
-        return solve(root, 0, targetSum, dp);;
+    int pathSum(TreeNode* root, int sum) {
+        if(!root) return 0;
+        int includingRoot = helper(root, sum);
+        int excludingRoot = pathSum(root->left, sum) + pathSum(root->right, sum);
+        return includingRoot + excludingRoot;
     }
 };
