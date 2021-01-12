15e2f625761a4ac530c88161bdd300038bc96c17
smmehrab <mehrab.24csedu.001@gmail.com>
2021-01-12 08:22:47 +0600

    ImplementStackUsingHeap.cpp - Solved

---
+++ codes/leetcode/ImplementingStackUsingHeap.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+class Stack{ 
+private:
+	int key; 
+	priority_queue<pair<int, int> > heap; 
+
+public: 
+	Stack() {
+        this->key = 0;
+    }
+
+	void push(int n) { 
+        key++; 
+        heap.push({key, n}); 
+    } 
+
+	void pop() { 
+        if(heap.empty()) cout<< "Heap is empty!" << endl;  
+        key--; 
+        heap.pop();
+    } 
+
+	int top() { 
+        pair<int, int> topPair = heap.top(); 
+        return topPair.second; 
+    } 
+
+	bool isEmpty() { 
+        return heap.empty(); 
+    } 
+}; 
+
+int main() { 
+	Stack* s = new Stack();
+
+	s->push(10); 
+	s->push(20); 
+	s->push(30); 
+	
+    while(!s->isEmpty()) { 
+		cout << s->top() << endl; 
+		s->pop(); 
+	}
+    
+    return 0; 
+} 
