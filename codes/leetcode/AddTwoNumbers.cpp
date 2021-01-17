0662642d1af8be8faf8e49330df5399c8ad95a53
smmehrab <mehrab.24csedu.001@gmail.com>
2021-01-17 06:33:00 +0600

    AddTwoNumbers.cpp - AC

---
+++ codes/leetcode/AddTwoNumbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *tail, *current, *head=nullptr;
+        int  d1, d2, s, c=0;
+              
+        while(l1 || l2) {
+            d1=0, d2=0;
+            if(l1) d1 = l1->val;
+            if(l2) d2 = l2->val;
+            s = d1+d2+c;          
+            c = (s>=10) ? 1 : 0; 
+            
+            current = new ListNode(s%10);
+            if(!head) head = current;
+            else tail->next = current;
+            tail = current;
+            
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+            
+        if(c>0) tail->next = new ListNode(c);
+        return head;
+    }
+};
