51f8f2060df9791cda43bd3fdf533ef7be50a1d5
smmehrab <mehrab.24csedu.001@gmail.com>
2021-01-18 22:28:09 +0600

    MaxNumberofKSumPairs.cpp - AC

---
+++ codes/leetcode/MaxNumberofKSumPairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int low=0, high=nums.size()-1, operations=0;
+        while(low<high) {
+            if(nums[low]+nums[high]==k) operations++, low++, high--;
+            else if(nums[low]+nums[high]<k) low++;
+            else if(nums[low]+nums[high]>k) high--;
+        }
+        return operations;
+    }
+};
