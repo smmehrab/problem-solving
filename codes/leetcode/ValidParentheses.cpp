585431145e491122f669fc8c9ea24fc068db3932
smmehrab <mehrab.24csedu.001@gmail.com>
2021-01-23 01:24:49 +0600

    ValidParentheses.cpp - AC

---
+++ codes/leetcode/ValidParentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c=='(' || c=='[' || c=='{') st.push(c);
+            else if (st.empty()) return false;
+            else if(c==')') {
+                if (st.top()=='(') st.pop();
+                else return false;   
+            }
+            else if(c=='}') {
+                if (st.top()=='{') st.pop();
+                else return false;   
+            }
+            else if(c==']') {
+                if (st.top()=='[') st.pop();
+                else return false;   
+            }
+        }
+        
+        return st.empty();
+    }
+};
