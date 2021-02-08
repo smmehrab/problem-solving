6a66ed2a7fff6fe7ab6979600f39fd13b9ac440e
smmehrab <mehrab.24csedu.001@gmail.com>
2021-02-08 12:10:44 +0600

    IsomorphicStrings.cpp - AC

---
+++ codes/leetcode/IsomorphicStrings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int n = s.size();
+        map<char, char> smt, tms;
+        for(int i=0; i<n; i++) {
+            if(smt.find(s[i])==smt.end() && tms.find(t[i])==tms.end()) {
+                smt.insert({s[i], t[i]});
+                tms.insert({t[i], s[i]});
+                continue;
+            }
+            if(tms.find(t[i])!=tms.end() && tms[t[i]] != s[i]) 
+                return false;
+            if(smt.find(s[i])!=smt.end() && smt[s[i]] != t[i]) 
+                return false;
+        }
+        return true;
+    }
+};
