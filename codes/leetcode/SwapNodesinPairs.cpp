ebbfd1f36d9d7e6888af53e9ffa2653f1c89a03e
smmehrab <mehrab.24csedu.001@gmail.com>
2021-02-12 19:09:27 +0600

    SwapNodesinPairs.cpp - AC

---
+++ codes/leetcode/SwapNodesinPairs.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* rest = swapPairs(head->next->next);
+        ListNode* first = head;
+        ListNode* second = head->next;
+        first->next = rest;
+        second->next = first;
+        return second;
+    }
+};
