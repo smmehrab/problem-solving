85638af3a37dea74f7ceea87f74cdf6e9675e56d
smmehrab <mehrab.24csedu.001@gmail.com>
2021-02-13 11:08:00 +0600

    TwoSumlV.cpp - AC

---
+++ codes/leetcode/TwoSumlV.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    bool findPair(TreeNode* root, int k, set<int> &nodes) {
+        if (!root) return false;
+        
+        // Searching Left Subtree
+        if (findPair(root->left, k, nodes)) return true;
+        
+        // Processing Current Node
+        if (nodes.find(k-root->val) != nodes.end()) return true;
+        else nodes.insert(root->val); 
+
+        // Searching Right Subtree
+        return findPair(root->right, k, nodes);
+    }
+public:
+    bool findTarget(TreeNode* root, int k) {
+        set<int> nodes;
+        return findPair(root, k, nodes);
+    }
+};
