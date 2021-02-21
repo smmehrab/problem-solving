53554dfe923cf5367008b3ef5232541571a5ec50
smmehrab <mehrab.24csedu.001@gmail.com>
2021-02-21 21:55:37 +0600

    UncrossedLines.cpp - AC

---
+++ codes/leetcode/UncrossedLines.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& a, vector<int>& b) {
+        int n=a.size(), m=b.size();
+        int dp[n+1][m+1];
+        for (int i=0; i<=n; i++) {
+            for (int j=0; j<=m; j++) {
+                if (i == 0 || j == 0) dp[i][j] = 0;
+                else if (a[i-1] == b[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[n][m];    
+    }
+};
