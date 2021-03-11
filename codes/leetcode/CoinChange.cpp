abf78f71488e5a965fe4885284079eb6c96fcb0f
smmehrab <mehrab.24csedu.001@gmail.com>
2021-03-11 16:21:06 +0600

    CoinChange.cpp - AC

---
+++ codes/leetcode/CoinChange.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        if(coins.size()==1) 
+            return (amount%coins[0]==0) ? amount/coins[0] : -1;
+        
+        vector<int> dp;
+        for(int i=0; i<=amount; i++) dp.push_back(100100);
+
+        dp[0] = 0;
+        for(int sum=1; sum<=amount; sum++) {
+            for(int coin : coins) {
+                if(sum>=coin)
+                    dp[sum] = min(dp[sum], dp[sum-coin]+1);
+            }
+        }       
+        
+        return (dp[amount]==100100) ? -1 : dp[amount];
+    }
+};
