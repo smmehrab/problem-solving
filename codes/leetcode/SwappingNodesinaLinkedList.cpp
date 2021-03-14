94194b1a510f1a1438b41361644225c285bb47ac
S.M.Mehrab <mehrab.24csedu.001@gmail.com>
2021-03-14 17:02:38 +0600

    SwappingNodesinaLinkedList.cpp - AC

---
+++ codes/leetcode/SwappingNodesinaLinkedList.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode* current = head;
+        ListNode* first;
+        ListNode* second = head;
+        
+        for (int i=0; i<k-1; i++) 
+            current = current->next; 
+        first = current; 
+        
+        current = current->next;        
+        while (current) { 
+            current = current->next;
+            second = second->next;
+        }
+        
+        int temp = first->val;
+        first->val = second->val;
+        second->val = temp;
+
+        return head;
+    }
+};
