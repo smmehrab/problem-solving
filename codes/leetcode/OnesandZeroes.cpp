a76425e414d440429985977c6d30caa6f7879f5d
S.M.Mehrab <mehrab.24csedu.001@gmail.com>
2021-04-02 19:49:56 +0600

    OnesandZeroes.cpp - AC

---
+++ codes/leetcode/OnesandZeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {        
+        vector<vector<int> > dp(n + 1, vector<int>(m + 1, 0));
+        int ones, zeros;
+        for (string str : strs) {
+            ones = count(str.begin(), str.end(), '1');
+            zeros = count(str.begin(), str.end(), '0');
+            for (int i=n; i>=ones; i--) {
+                for (int j=m; j>=zeros; j--)
+                    dp[i][j] = max(dp[i][j], 1+dp[i-ones][j-zeros]);   
+            }
+        }
+        return dp[n][m];
+    }
+};
