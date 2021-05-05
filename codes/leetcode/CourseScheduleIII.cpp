1f66bd9434b340680361899589edab3b6b7532c2
S.M.Mehrab <mehrab.24csedu.001@gmail.com>
2021-05-05 18:38:51 +0600

    CourseScheduleIII.cpp - AC

---
+++ codes/leetcode/CourseScheduleIII.cpp
@@ -0,0 +1,48 @@
+class Solution {    
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+        sort(courses.begin(), courses.end(), [] (vector<int> &a, vector<int> &b) {
+            return a[1]==b[1] ? a[0]<b[0] : a[1]<b[1];
+        });
+        
+        // O (n*n)
+        //         int n = courses.size();
+        //         int time=0, courseTaken=0;
+        //         for (int i=0; i<n; i++) {
+        //             if (time+courses[i][0] <= courses[i][1]) {
+        //                 time += courses[i][0];
+        //                 courses[courseTaken++] = courses[i];
+        //             } 
+
+        //             else {
+        //                 int courseWithMaximumDurationSoFar = i;
+        //                 for (int j=0; j<courseTaken; j++) {
+        //                     if (courses[j][0] > courses[courseWithMaximumDurationSoFar][0])
+        //                         courseWithMaximumDurationSoFar = j;
+        //                 }
+        //                 if (courses[courseWithMaximumDurationSoFar][0] > courses[i][0]) {
+        //                     time += courses[i][0]-courses[courseWithMaximumDurationSoFar][0];
+        //                     courses[courseWithMaximumDurationSoFar] = courses[i];
+        //                 }
+        //             }
+        //         }
+
+        //         return courseTaken;
+        
+        // O (nLogn)
+        priority_queue<int> pq;
+        int time = 0;
+        for (vector<int> course : courses) {
+            if (time+course[0] <= course[1]) {
+                time += course[0];
+                pq.push(course[0]);                
+            }
+            else if (!pq.empty() && pq.top()>course[0]) {
+                time += course[0]-pq.top(); 
+                pq.pop();
+                pq.push(course[0]);
+            }
+        }
+        return pq.size();
+    }
+};
