21d2ba999c11fe10511693509266c6d1c12b7837
S.M.Mehrab <mehrab.24csedu.001@gmail.com>
2021-05-21 12:30:56 +0600

    1137ExpandingRods.cpp - AC

--- codes/lightoj/1137ExpandingRods.cpp
+++ codes/lightoj/1137ExpandingRods.cpp
@@ -1,42 +1,46 @@
-#include <bits/stdc++.h>
-#include <iostream>
-#include <cmath>
+#include<bits/stdc++.h>
 using namespace std;
+
 #define PRECISION 0.00000001
 
-int main()
-{
-    int testCaseCount;
-    double l, n, c, el, h, a, low, high, s;
-    cin >> testCaseCount;
-    for (int testCase = 1; testCase <= testCaseCount; testCase++)
-    {
-        cin >> l >> n >> c;
-        if (n == 0)
-            cout << "Case " << testCase << ": 0" << endl;
-        else
-        {
-            el = (1 + (n * c)) * l;
-            low = 0.00;
-            high = l;
-            while (high - low > PRECISION)
-            {
-                h = (low + high) / 2.00;
-                a = (l * l) / (4.00 * h);
-                s = (a + h) * atan(l / (a - h));
+int main() {
+    cout << fixed << setprecision(10);
 
-                if (s > el)
-                {
-                    high = h;
-                }
+    int testCaseCount;
+    double l1, n, C, l2, estimatedL2;
+    double h=0;
+    double low, high;
+    double a, b, c, r, theta;
+
+    cin >> testCaseCount;
+    for (int testCase=1; testCase<=testCaseCount; testCase++, h=0) {
+        cin >> l1 >> n >> C;
+        
+        if(n != 0) {
+            l2 = (1 + (n*C)) * l1;
+            c = l1/2.00;
+
+            low = 0.00;
+            high = l1;
+            while(high-low > PRECISION) {
+                b = (low+high) / 2.00;
+
+                a = (c*c)/(1.00*b);
+                r = (a+b)/2.00;
+                theta = 2.00 * asin(c/r);
+                estimatedL2 = r * theta;
+
+                if(estimatedL2 > l2)
+                    high = b;
                 else
-                {
-                    low = h;
-                }
+                    low = b;
             }
-            cout << "Case " << testCase << ": ";
-            cout << fixed << setprecision(6) << h << endl;
+
+            h = b;
         }
+
+        cout << "Case " << testCase << ": " << h << endl;
     }
+
     return 0;
 }
