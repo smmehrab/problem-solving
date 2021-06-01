0aeff709e270a1df7ae7466590064433ffe1ec2b
S.M.Mehrab <mehrab.24csedu.001@gmail.com>
2021-06-01 08:35:02 +0600

    FloydWarshall.cpp - Modified

---
+++ codes/FloydWarshall.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define INF INT_MAX
+
+void floydWarshall(int n, vector<vector<int>>& distances) {
+	for(int k=0; k<n; k++) {
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				if(distances[i][k] != INF && distances[k][j] != INF && distances[i][k] + distances[k][j] < distances[i][j])
+					distances[i][j] = distances[i][k] + distances[k][j];
+			}
+		}
+	}
+}
+
+int main(){
+	int n, m, q, a, b, c;
+    cin >> n >> m >> q;
+
+    vector<vector<int>> distances(n, vector<int>(n, INF));
+    for(int i=0; i<n; i++)
+        distances[i][i] = 0;
+
+    for(int i=0; i<m; i++) {
+        cin >> a >> b >> c;
+        a--; b--;
+        distances[a][b] = c;
+        distances[b][a] = c;
+    }
+
+	floydWarshall(n, distances);
+
+	cout<<"Shortest Distances Between Every Pair of Vertices" << endl;
+	for (int i=0; i<n; i++) {
+		for (int j=0; j<n; j++) {
+            cout << ((distances[i][j] == INF) ? "-1" : to_string(distances[i][j])) << "\t";
+		}
+		cout<<endl;
+	}
+
+	return 0;
+}
