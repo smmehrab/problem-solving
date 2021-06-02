c2a864b112f290723dda63bf11d5f53064d4daaf
S.M.Mehrab <mehrab.24csedu.001@gmail.com>
2021-06-02 09:06:33 +0600

    1076GettheContainers.cpp - AC

--- codes/lightoj/1076GettheContainers.cpp
+++ codes/lightoj/1076GettheContainers.cpp
@@ -7,3 +7,62 @@ institute   :   university of dhaka, bangladesh
 session     :   2017-2018
 ************************************************
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+#define INF INT_MAX
+
+int estimate(int capacity, vector<int>& vessels) {
+    int container=0, count=1;
+    for(int vessel : vessels) {
+        if(vessel>capacity) 
+            return INF;
+        else if(container+vessel<=capacity) 
+            container += vessel;
+        else {
+            count++;
+            container = vessel;
+        }
+    }
+    return count;
+}
+
+int binarySearch(int n, int m, vector<int>& vessels) {
+    int low = -1, high = 0;
+    for(int vessel : vessels) {
+        low = max(low, vessel);
+        high += vessel;
+    }
+
+    if(m==1) 
+        return high;
+    if(n==m)
+        return low;
+
+    int capacity, minCapacity, containerCount;
+    while(low<high) {
+        capacity = (low + (high-low)/2);
+        containerCount = estimate(capacity, vessels);
+        if(containerCount > m) 
+            low = capacity+1;
+        else 
+            minCapacity = high = capacity;
+    }
+    return minCapacity;
+}
+
+int main() {
+    int testCaseCount, n, m, minCapacity;
+    cin >> testCaseCount;
+    for(int testCase=1; testCase<=testCaseCount; testCase++) {
+        cin >> n >> m;
+
+        vector<int> vessels(n);
+        for(int &vessel : vessels) 
+            cin >> vessel;
+
+        minCapacity = binarySearch(n, m, vessels);
+        cout << "Case " << testCase << ": " << minCapacity << endl;
+    }
+    return 0;
+}
