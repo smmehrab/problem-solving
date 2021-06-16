8c171b479f55e2571e049c029fc67f63fdddde01
S.M.Mehrab <mehrab.24csedu.001@gmail.com>
2021-06-16 23:57:07 +0600

    BookShop.cpp - AC

--- codes/cses/DynamicProgramming/BookShop.cpp
+++ codes/cses/DynamicProgramming/BookShop.cpp
@@ -10,50 +10,34 @@ session     :   2017-2018
 #include <bits/stdc++.h>
 using namespace std;
 
-long long int n, x;
-long long int dp[1010];
+int main() {    
+    int n, x, option1, option2;
 
-long long int solve(int moneyLeft, vector <int>& costs, vector <int>& pages){
-    if (moneyLeft==0) return 0;
-    
-    long long int &ret = dp[moneyLeft];
-    if (ret!=-1) return ret;
-    
-    ret = -1;
-    for (int i=0; i<n; i++){
-        if(costs[i]<=moneyLeft)
-            ret = max(ret, pages[i]+solve(moneyLeft-costs[i], costs, pages));
-        else break;
-    }        
-    
-    return ret;
-}
- 
-int main(){
-    memset(dp,-1,sizeof (dp));
-    
     cin >> n >> x;
-    vector <int> costs(n);
+    vector<int> costs(n);
+    vector<int> pages(n);
+
     for (int &cost : costs)        
         cin >> cost;
-    vector <int> pages(n);
     for(int &page : pages)
         cin >> page;
 
-    sort(costs.begin(), costs.end());
-    cout << solve(x, costs, pages) << endl;
-
-    vector<vector<int>> dp(n+1,vector<int>(x+1,0));
-    for (int i = 1; i <= n; i++) {
-        for (int j = 0; j <= x; j++) {
-        dp[i][j] = dp[i-1][j];
-        int left = j-price[i-1];
-        if (left >= 0) {
-                dp[i][j] = max(dp[i][j], dp[i-1][left]+pages[i-1]);
-        }
+    /*
+        Option1 --> Not Taking Current Book
+        Option2 --> Taking Current Book
+    */
+    vector<vector<int>> dp(n+1, vector<int>(x+1, 0));
+    dp[0][0] = 0;
+    for(int book=1; book<=n; book++) {
+        int cost = costs[book-1];
+        int page = pages[book-1];
+        for(int money=0; money<=x; money++) {
+            option1 = dp[book-1][money];
+            option2 = (money>=cost) ? dp[book-1][money-cost] + page : 0;
+            dp[book][money] = max(option1, option2);
         }
     }
-    cout << dp[n][x] << endl;
 
+    cout << dp[n][x] << endl;
     return 0;
 }
