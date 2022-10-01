a00f22bef84458036b2414ff499bebf79b845a7e
smmehrab <mehrab.24csedu.001@gmail.com>
2022-10-01 06:48:19 +0600

    MedianofTwoSortedArrays - AC - Binary Search - O(log(min(n,m)))

--- codes/leetcode/MedianofTwoSortedArrays.py
+++ codes/leetcode/MedianofTwoSortedArrays.py
@@ -1,3 +1,11 @@
+# **************************************************
+# username    :   smmehrab
+# fullname    :   s.m.mehrabul islam
+# email       :   smmehrabul-2017614964@cs.du.ac.bd
+# institute   :   university of dhaka, bangladesh
+# reg         :   2017614964
+# **************************************************
+
 class MedianFinder:
 
     def __init__(self):
@@ -89,12 +97,11 @@ class MedianFinder:
           m2 = self.quickselect(data, (data_size/2))
           return (0.5) * (m1+m2)
 
-class Solution(object):    
-    def findMedianSortedArrays(self, nums1, nums2):
+class Solution(object):
+
+    def solveUsingMedianOfMedians(self, nums1, nums2):
         """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: float
+            O(n+m)
         """
 
         # merging
@@ -112,5 +119,62 @@ class Solution(object):
         # implemented inside MedianFinder class
         medianfinder = MedianFinder()
         median = medianfinder.find(nums)
+        return median
 
+    def solveUsingBinarySearch(self, a, b):
+        """
+            O(log(min(a,b)))
+        """
+
+        aLen = len(a)
+        bLen = len(b)
+        n = aLen + bLen
+        nBy2 = n//2
+
+        if bLen < aLen:
+            a, b = b, a
+            aLen, bLen = bLen, aLen
+
+        median = -1
+
+        # binary search
+        l = 0
+        r = aLen-1
+        while True:
+            ai = (l+r)//2
+            bi = nBy2-ai-2
+
+            aLeft = a[ai] if ai>=0 else float("-infinity")
+            aRight = a[ai+1] if (ai+1)<aLen else float("infinity") 
+            bLeft = b[bi] if bi>=0 else float("-infinity")
+            bRight = b[bi+1] if (bi+1)<bLen else float("infinity")
+
+            # valid partitioning
+            if aLeft<=bRight and bLeft<=aRight:
+                if n&1 == 1:
+                    median = min(aRight, bRight)
+                else:
+                    median = (0.5*(max(aLeft, bLeft) + min(aRight, bRight)))
+                break
+            # invalid partitioning
+            elif aLeft>bRight:
+                r = ai-1
+            elif bLeft>aRight:
+                l = ai+1
+
+        return median
+
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+
+        # O(n+m)
+        # median = self.solveUsingMedianOfMedians(nums1, nums2)
+
+        # O(log(min(n, m)))
+        median = self.solveUsingBinarySearch(nums1, nums2)
+        
         return median
