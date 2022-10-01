3559bba2b8ef689bcfb3f80d7a087c11a4508f42
smmehrab <mehrab.24csedu.001@gmail.com>
2022-10-01 07:26:36 +0600

    kmp.py - modified

--- codes/algorithms/string_matching/kmp.py
+++ codes/algorithms/string_matching/kmp.py
@@ -1,7 +1,7 @@
 # ************************************************
 # username  :   smmehrab
 # fullname  :   s.m.mehrabul islam
-# email     :   mehrab.24csedu.001@gmail.com
+# email     :   smmehrabul-2017614964@cs.du.ac.bd
 # institute :   university of dhaka, bangladesh
 # reg       :   2017614964
 # ************************************************
