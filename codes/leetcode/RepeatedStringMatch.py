89a393f8722b2d13925f19d89ccd5d68a0588196
smmehrab <mehrab.24csedu.001@gmail.com>
2022-10-01 07:51:55 +0600

    RepeatedStringMatch - AC

---
+++ codes/leetcode/RepeatedStringMatch.py
@@ -0,0 +1,77 @@
+# ************************************************
+# username  :   smmehrab
+# fullname  :   s.m.mehrabul islam
+# email     :   smmehrabul-2017614964@cs.du.ac.bd
+# institute :   university of dhaka, bangladesh
+# reg       :   2017614964
+# ************************************************
+
+class KMP:
+
+    def __init__(self, pattern):
+        self.pattern = pattern
+        self.lps = [0]*len(pattern)
+        self.computeLPS()
+        pass
+
+    def computeLPS(self):
+        m = len(self.pattern)
+        left = 0
+        right = 1
+        self.lps[left] = 0
+        while right<m:
+            if self.pattern[left] == self.pattern[right]:
+                self.lps[right] = left+1
+                left += 1
+                right += 1
+            else:
+                if left != 0:
+                    left = self.lps[left-1]
+                else:
+                    self.lps[right] = 0
+                    right += 1
+
+    def isSubstringOf(self, text):
+        n = len(text)
+        m = len(self.pattern)
+        tIndex = 0
+        pIndex = 0
+        while tIndex<n:
+            if text[tIndex] == self.pattern[pIndex]:
+                tIndex += 1
+                pIndex += 1
+            else:
+                if pIndex!=0:
+                    pIndex = self.lps[pIndex-1]
+                else:
+                    tIndex += 1
+            if pIndex == m:
+                return True
+
+        return False
+
+class Solution(object):
+    def repeatedStringMatch(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+        matcher = KMP(b)
+
+        count = 1
+        original_a = a
+        while len(a)<len(b):
+            a += original_a
+            count += 1
+            
+        if matcher.isSubstringOf(a):
+            return count
+        
+        a += original_a
+        count += 1
+        
+        if matcher.isSubstringOf(a):
+            return count
+
+        return -1
