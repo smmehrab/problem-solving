59fb6f17e781db2b04794770dcdb68645d8590c9
smmehrab <mehrab.24csedu.001@gmail.com>
2022-10-08 13:31:01 +0600

    BulbSwitcher - AC

---
+++ codes/leetcode/BulbSwitcher.py
@@ -0,0 +1,17 @@
+# ************************************************
+# username  :   smmehrab
+# fullname  :   s.m.mehrabul islam
+# email     :   smmehrabul-2017614964@cs.du.ac.bd
+# institute :   university of dhaka, bangladesh
+# reg       :   2017614964
+# ************************************************
+
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return int(math.sqrt(n))
+        
+        
