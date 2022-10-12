7b6cd773786879f6059b6aaf6b88f96115344cd0
smmehrab <mehrab.24csedu.001@gmail.com>
2022-10-12 06:07:28 +0600

    LargestPerimeterTriangle - AC

---
+++ codes/leetcode/LargestPerimeterTriangle.py
@@ -0,0 +1,20 @@
+# ************************************************
+# username  :   smmehrab
+# fullname  :   s.m.mehrabul islam
+# email     :   smmehrabul-2017614964@cs.du.ac.bd
+# institute :   university of dhaka, bangladesh
+# reg       :   2017614964
+# ************************************************
+
+class Solution(object):
+    def largestPerimeter(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        for i in xrange(len(nums) - 3, -1, -1):
+            if nums[i] + nums[i+1] > nums[i+2]:
+                return nums[i] + nums[i+1] + nums[i+2]
+        return 0
+    
