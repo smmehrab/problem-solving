d3bce10236a420a78374b01dfe078b5717662f40
smmehrab <mehrab.24csedu.001@gmail.com>
2022-10-13 12:14:34 +0600

    DeleteNodeinaLinkedList - AC

---
+++ codes/leetcode/DeleteNodeinaLinkedList.py
@@ -0,0 +1,25 @@
+# ************************************************
+# username  :   smmehrab
+# fullname  :   s.m.mehrabul islam
+# email     :   smmehrabul-2017614964@cs.du.ac.bd
+# institute :   university of dhaka, bangladesh
+# reg       :   2017614964
+# ************************************************
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        next_node = node.next
+        node.val = next_node.val
+        node.next = next_node.next
+        next_node.next = None
+        del(next_node)
