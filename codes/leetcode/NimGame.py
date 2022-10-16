9197a1f8fa3d5faaf81e0166a7b5443e6c3adc47
smmehrab <mehrab.24csedu.001@gmail.com>
2022-10-16 21:23:08 +0600

    NimGame - AC

---
+++ codes/leetcode/NimGame.py
@@ -0,0 +1,15 @@
+# ************************************************
+# username  :   smmehrab
+# fullname  :   s.m.mehrabul islam
+# email     :   smmehrabul-2017614964@cs.du.ac.bd
+# institute :   university of dhaka, bangladesh
+# reg       :   2017614964
+# ************************************************
+
+class Solution(object):
+    def canWinNim(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return (n%4)
