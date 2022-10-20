931b40d3ea96fe5746edcfb45129de41ca9be755
smmehrab <mehrab.24csedu.001@gmail.com>
2022-10-20 06:24:16 +0600

    IntegertoRoman - AC

---
+++ codes/leetcode/IntegertoRoman.py
@@ -0,0 +1,22 @@
+# ************************************************
+# username  :   smmehrab
+# fullname  :   s.m.mehrabul islam
+# email     :   smmehrabul-2017614964@cs.du.ac.bd
+# institute :   university of dhaka, bangladesh
+# reg       :   2017614964
+# ************************************************
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        
+        keys = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        
+        roman = ""
+        for i, value in enumerate(values):
+            roman += keys[i] * (num // value)
+            num %= value
+            if num == 0:
+                break
+            
+        return roman
