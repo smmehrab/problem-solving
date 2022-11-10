dfd92338c986b77f7e7c6bf58aeec6190b1c94fd
smmehrab <mehrab.24csedu.001@gmail.com>
2022-11-10 06:49:03 +0600

    RemoveAllAdjacentDuplicatesInString - modified

--- codes/leetcode/RemoveAllAdjacentDuplicatesInString.py
+++ codes/leetcode/RemoveAllAdjacentDuplicatesInString.py
@@ -6,61 +6,18 @@
 # reg       :   2017614964
 # ************************************************
 
-class Node:
-	def __init__(self, value):
-		self.value = value
-		self.next = None
-
-class Stack:
-
-	def __init__(self):
-		self.head = Node("head")
-		self.__size = 0
-
-	def __str__(self):
-		node = self.head.next
-		result = ""
-		while node:
-			result += str(node.value) + "->"
-			node = node.next
-		return result[:-3]
-
-	def size(self):
-		return self.__size
-
-	def empty(self):
-		return self.__size == 0
-
-	def peek(self):
-		if self.empty():
-			raise Exception("Can't Peek from Empty Stack")
-		return self.head.next.value
-
-	def push(self, value):
-		node = Node(value)
-		node.next = self.head.next
-		self.head.next = node
-		self.__size += 1
-
-	def pop(self):
-		if self.empty():
-			raise Exception("Can't Pop from Empty Stack")
-		node = self.head.next
-		self.head.next = self.head.next.next
-		self.__size -= 1
-		return node.value
+from collections import deque
 
 class Solution:
     def removeDuplicates(self, s: str) -> str:
-        stack = Stack()
+        stack = deque()
         for c in s:
-            if stack.empty() or stack.peek()!=c:
-                stack.push(c)
-            elif stack.peek()==c:
+            if not stack or stack[-1]!=c:
+                stack.append(c)
+            elif stack[-1]==c:
                 stack.pop()
         set_s = ""
-        while not stack.empty():
+        while stack:
             set_s += stack.pop()
         set_s = set_s[::-1]
         return set_s
-
