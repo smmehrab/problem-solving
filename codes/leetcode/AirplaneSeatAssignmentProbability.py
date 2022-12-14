9bbfcfea94b00e44b58e73ea0638eda1e0edd476
smmehrab <mehrab.24csedu.001@gmail.com>
2022-12-14 18:59:21 +0600

    AirplaneSeatAssignmentProbability - AC

---
+++ codes/leetcode/AirplaneSeatAssignmentProbability.py
@@ -0,0 +1,11 @@
+# ************************************************
+# username  :   smmehrab
+# fullname  :   s.m.mehrabul islam
+# email     :   smmehrabul-2017614964@cs.du.ac.bd
+# institute :   university of dhaka, bangladesh
+# reg       :   2017614964
+# ************************************************
+
+class Solution:
+    def nthPersonGetsNthSeat(self, n: int) -> float:
+        return 1 if n==1 else 0.5
