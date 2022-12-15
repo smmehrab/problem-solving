6493588826ed978b5903daa25a32b89283cb1377
smmehrab <mehrab.24csedu.001@gmail.com>
2022-12-15 07:17:11 +0600

    LongestCommonSubsequence - AC

---
+++ codes/leetcode/LongestCommonSubsequence.py
@@ -0,0 +1,20 @@
+# ************************************************
+# username  :   smmehrab
+# fullname  :   s.m.mehrabul islam
+# email     :   smmehrabul-2017614964@cs.du.ac.bd
+# institute :   university of dhaka, bangladesh
+# reg       :   2017614964
+# ************************************************
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n1 = len(text1)
+        n2 = len(text2)
+        dp = [[0 for j in range(n2+1)] for i in range(n1+1)]
+        for i in range(n1-1, -1, -1):
+            for j in range(n2-1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+        return dp[0][0]
