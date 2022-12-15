638d2541354b950dc57e939f6f1e416672077310
smmehrab <mehrab.24csedu.001@gmail.com>
2022-12-15 20:24:33 +0600

    PascalsTriangle - AC

---
+++ codes/leetcode/PascalsTriangle.py
@@ -0,0 +1,17 @@
+# ************************************************
+# username  :   smmehrab
+# fullname  :   s.m.mehrabul islam
+# email     :   smmehrabul-2017614964@cs.du.ac.bd
+# institute :   university of dhaka, bangladesh
+# reg       :   2017614964
+# ************************************************
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
+        for i in range(numRows):
+            row = [1]*(i+1)
+            for j in range(1, i):
+                row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+            triangle.append(row)
+        return triangle
