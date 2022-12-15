12e3392497464f65e31920a7ff3325fcce7eb3bf
smmehrab <mehrab.24csedu.001@gmail.com>
2022-12-15 20:13:56 +0600

    MiddleoftheLinkedList - AC

---
+++ codes/leetcode/MiddleoftheLinkedList.py
@@ -0,0 +1,22 @@
+# ************************************************
+# username  :   smmehrab
+# fullname  :   s.m.mehrabul islam
+# email     :   smmehrabul-2017614964@cs.du.ac.bd
+# institute :   university of dhaka, bangladesh
+# reg       :   2017614964
+# ************************************************
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
