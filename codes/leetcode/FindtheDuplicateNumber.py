bd98bd9d50c5f4f31744ee8b6f1fb371ebb363de
smmehrab <smmehrabul-2017614964@cs.du.ac.com>
2023-09-19 07:48:03 +0600

    FindtheDuplicateNumber - AC

---
+++ codes/leetcode/FindtheDuplicateNumber.py
@@ -0,0 +1,26 @@
+# ************************************************
+# username  :   smmehrab
+# fullname  :   s.m.mehrabul islam
+# email     :   smmehrabul-2017614964@cs.du.ac.bd
+# institute :   university of dhaka, bangladesh
+# reg       :   2017614964
+# ************************************************
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        """
+            Floyd's Cycle Detection
+        """
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow = 0
+        while True:
+            slow = nums[slow]
+            fast = nums[fast]
+            if slow == fast:
+                break
+        return slow
