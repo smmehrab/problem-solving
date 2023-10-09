8d433a2d2399027ff94637e23f00891a45f414f7
smmehrab <smmehrabul-2017614964@cs.du.ac.bd>
2023-10-09 16:24:58 +0600

    FindifPathExistsinGraph - modified

--- codes/leetcode/FindifPathExistsinGraph.py
+++ codes/leetcode/FindifPathExistsinGraph.py
@@ -5,13 +5,6 @@
 # institute :   university of dhaka, bangladesh
 # reg       :   2017614964
 # ************************************************
-# ************************************************
-# username  :   smmehrab
-# fullname  :   s.m.mehrabul islam
-# email     :   smmehrabul-2017614964@cs.du.ac.bd
-# institute :   university of dhaka, bangladesh
-# reg       :   2017614964
-# ************************************************
 
 from collections import defaultdict, deque
 
