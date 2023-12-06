dc5e448d2bcf89e3924118776149fa87f51194a2
smmehrab <smmehrabul-2017614964@cs.du.ac.bd>
2023-12-06 19:37:39 +0600

    modified

--- codes/interview_prep/endoscope.cpp
+++ codes/interview_prep/endoscope.cpp
@@ -6,64 +6,81 @@
 using namespace std;
 
 int grid[50][50], n, m, R, C, L;
-int visited[50][50]={0};
+int visited[50][50] = {0};
 
-void dfs(int x, int y, int l){
+void backtrack(int x, int y, int l){
     if(x<0 || x>=n || y<0 || y>=m || l==0)
         return;
-    visited[x][y]=1;
-    if(grid[x][y] == 1){
-        if((x!=0) && (grid[x-1][y] ==2 || grid[x-1][y] ==5 || grid[x-1][y] ==6 || grid[x-1][y] ==1))  //up
-            dfs(x-1, y, l-1);
-        if((x!=n-1) && (grid[x+1][y] ==2 || grid[x+1][y] ==4 || grid[x+1][y] ==7 || grid[x+1][y] ==1)) //down
-            dfs(x+1, y, l-1);
-
-        if((y!=0)&& (grid[x][y-1] ==3 || grid[x][y-1] ==4 || grid[x][y-1] ==5 || grid[x][y-1] ==1))     //left
-            dfs(x, y-1, l-1);
-        if((y!=m-1) && (grid[x][y+1] ==3 || grid[x][y+1] ==6 || grid[x][y+1] ==7 || grid[x][y+1] ==1))   //right
-            dfs(x, y+1, l-1);
-        
+    // cout << "x: " << x << " y: " << y << " l: " << l << endl;
+    visited[x][y] = 1;
+    // up-down-left-right
+    if(grid[x][y] == 1) {
+        // up
+        if((x!=0) && (grid[x-1][y] ==2 || grid[x-1][y] ==5 || grid[x-1][y] ==6 || grid[x-1][y] ==1))
+            backtrack(x-1, y, l-1);
+        // down
+        if((x!=n-1) && (grid[x+1][y] ==2 || grid[x+1][y] ==4 || grid[x+1][y] ==7 || grid[x+1][y] ==1))
+            backtrack(x+1, y, l-1);
+        // left
+        if((y!=0)&& (grid[x][y-1] ==3 || grid[x][y-1] ==4 || grid[x][y-1] ==5 || grid[x][y-1] ==1))
+            backtrack(x, y-1, l-1);
+        // right
+        if((y!=m-1) && (grid[x][y+1] ==3 || grid[x][y+1] ==6 || grid[x][y+1] ==7 || grid[x][y+1] ==1))
+            backtrack(x, y+1, l-1);
     }
-    else if(grid[x][y] == 2){
-        if((x!=0) && (grid[x-1][y] ==1 || grid[x-1][y] ==5 || grid[x-1][y] ==6 || grid[x-1][y] ==2))    //up
-            dfs(x-1, y, l-1);
-        if((x!=n-1) && (grid[x+1][y] ==1 || grid[x+1][y] ==4 || grid[x+1][y] ==7 || grid[x+1][y] ==2))  //down
-            dfs(x+1, y, l-1);
-        
+    // up-down
+    else if(grid[x][y] == 2) {
+        // up
+        if((x!=0) && (grid[x-1][y] ==1 || grid[x-1][y] ==5 || grid[x-1][y] ==6 || grid[x-1][y] ==2))
+            backtrack(x-1, y, l-1);
+        // down
+        if((x!=n-1) && (grid[x+1][y] ==1 || grid[x+1][y] ==4 || grid[x+1][y] ==7 || grid[x+1][y] ==2))
+            backtrack(x+1, y, l-1);
     }
-    else if(grid[x][y] == 3){
-        if((y!=0)&& (grid[x][y-1] ==1 || grid[x][y-1] ==4 || grid[x][y-1] ==5 || grid[x][y-1] ==3))     //left
-            dfs(x, y-1, l-1);
-        if((y!=m-1) && (grid[x][y+1] ==1 || grid[x][y+1] ==6 || grid[x][y+1] ==7 || grid[x][y+1] ==3))   //right
-            dfs(x, y+1, l-1);
-        
+    // left-right
+    else if(grid[x][y] == 3) {
+        // left
+        if((y!=0)&& (grid[x][y-1] ==1 || grid[x][y-1] ==4 || grid[x][y-1] ==5 || grid[x][y-1] ==3))
+            backtrack(x, y-1, l-1);
+        // right
+        if((y!=m-1) && (grid[x][y+1] ==1 || grid[x][y+1] ==6 || grid[x][y+1] ==7 || grid[x][y+1] ==3))
+            backtrack(x, y+1, l-1);
     }
-    else if(grid[x][y] == 4){
-        if((x!=0) && (grid[x-1][y] ==1 || grid[x-1][y] ==2 || grid[x-1][y] ==5 || grid[x-1][y] ==6))    //up
-            dfs(x-1, y, l-1);  
-        if((y!=m-1) && (grid[x][y+1] ==1 || grid[x][y+1] ==3 || grid[x][y+1] ==6 || grid[x][y+1] ==7))   //right
-            dfs(x, y+1, l-1);
-        
+    // up-right
+    else if(grid[x][y] == 4) {
+        // up
+        if((x!=0) && (grid[x-1][y] ==1 || grid[x-1][y] ==2 || grid[x-1][y] ==5 || grid[x-1][y] ==6))
+            backtrack(x-1, y, l-1);
+        // right
+        if((y!=m-1) && (grid[x][y+1] ==1 || grid[x][y+1] ==3 || grid[x][y+1] ==6 || grid[x][y+1] ==7))
+            backtrack(x, y+1, l-1);
     }
-    else if(grid[x][y] == 5){
-        if((x!=n-1) && (grid[x+1][y] =1 || grid[x+1][y] ==2 || grid[x+1][y] ==7 || grid[x+1][y] ==4))  //down
-            dfs(x+1, y, l-1);
-        if((y!=m-1) && (grid[x][y+1] ==1 || grid[x][y+1] ==3 || grid[x][y+1] ==6 || grid[x][y+1] ==7))   //right
-            dfs(x, y+1, l-1);
-        
+    // down-right
+    else if(grid[x][y] == 5) {
+        // down
+        if((x!=n-1) && (grid[x+1][y] =1 || grid[x+1][y] ==2 || grid[x+1][y] ==7 || grid[x+1][y] ==4))
+            backtrack(x+1, y, l-1);
+        // right
+        if((y!=m-1) && (grid[x][y+1] ==1 || grid[x][y+1] ==3 || grid[x][y+1] ==6 || grid[x][y+1] ==7))
+            backtrack(x, y+1, l-1);
     }
-    else if(grid[x][y] == 6){
-        if((x!=n-1) && (grid[x+1][y] ==1 || grid[x+1][y] ==2 || grid[x+1][y] ==7 || grid[x+1][y] ==4))  //down
-            dfs(x+1, y, l-1);
-        if((y!=0)&& (grid[x][y-1] ==1 || grid[x][y-1] ==3 || grid[x][y-1] ==5 || grid[x][y-1] ==4))     //left
-            dfs(x, y-1, l-1);
-        
+    // down-left
+    else if(grid[x][y] == 6) {
+        // down
+        if((x!=n-1) && (grid[x+1][y] ==1 || grid[x+1][y] ==2 || grid[x+1][y] ==7 || grid[x+1][y] ==4))
+            backtrack(x+1, y, l-1);
+        // left
+        if((y!=0)&& (grid[x][y-1] ==1 || grid[x][y-1] ==3 || grid[x][y-1] ==5 || grid[x][y-1] ==4))
+            backtrack(x, y-1, l-1);
     }
-    else if(grid[x][y] == 7){
-        if((x!=0) && (grid[x-1][y] ==1 || grid[x-1][y] ==2 || grid[x-1][y] ==5 || grid[x-1][y] ==6))    //up
-            dfs(x-1, y, l-1);
-        if((y!=0)&& (grid[x][y-1] ==1 || grid[x][y-1] ==3 || grid[x][y-1] ==4 || grid[x][y-1] ==5))     //left
-            dfs(x, y-1, l-1);
+    // up-left
+    else if(grid[x][y] == 7) {
+        // up
+        if((x!=0) && (grid[x-1][y] ==1 || grid[x-1][y] ==2 || grid[x-1][y] ==5 || grid[x-1][y] ==6))
+            backtrack(x-1, y, l-1);
+        // left
+        if((y!=0)&& (grid[x][y-1] ==1 || grid[x][y-1] ==3 || grid[x][y-1] ==4 || grid[x][y-1] ==5))
+            backtrack(x, y-1, l-1);
     }
 }
 
@@ -78,7 +95,7 @@ int main() {
                 cin>>grid[i][j];
         }
 
-        dfs(R,C,L);
+        backtrack(R,C,L);
 
         int count=0;
         for(int i=0;i<n;i++){
